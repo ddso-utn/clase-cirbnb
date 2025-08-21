caa3ca8614c06c7c83b8b5917c033ff4b90bbaa9
Juan Pablo Castiglione <juanpablocastiglione01@gmail.com>
2025-08-21 13:13:50 -0300

    codigo chequeo reservas

--- domain.js
+++ domain.js
@@ -11,10 +11,17 @@ class Alojamiento {
     this.nombre = nombre;
     this.precioPorNoche = precioPorNoche;
     this.categoria = categoria;
+    this.reservas = [];
   }
 
   getDescripcion() {
     return `${this.nombre} (${this.categoria}) - ${this.precioPorNoche} por noche`;
+  }
+
+  tieneConflictoConFechas(fechaInicio, fechaFin) {
+    return this.reservas.some(reserva => {
+      return fechaInicio < reserva.diaFin && fechaFin > reserva.diaInicio;
+    });
   }
 }
 
